8179857ce78b3b33f6c76e7fdf8cb26c04ae1c43
Yell0wF1sh <86252839+Yell0wF1sh@users.noreply.github.com>
2024-09-29 19:36:14 -0400

    changed sql file

--- src/create-table.sql
+++ src/create-table.sql
@@ -1,10 +1,23 @@
+-- noinspection SqlDialectInspectionForFile
+
+-- noinspection SqlNoDataSourceInspectionForFile
+
 drop schema if exists db_project_resource;
 
 create schema db_project_resource;
 
 use db_project_resource;
 
-create table projects(
+create table if not exists languages(
+    language_id             int NOT NULL,
+    language_name           nvarchar(100),
+    compiler_invocation     nvarchar(500),
+    create_date      DATETIME DEFAULT NOW(),
+    primary key (language_id),
+    UNIQUE (language_name)
+);
+
+create table if not exists projects(
     project_id       int NOT NULL,
     owner_user_id    int,
     project_name     nvarchar(100),
@@ -12,17 +25,10 @@ create table projects(
     create_date      DATETIME DEFAULT NOW(),
     primary key (project_id),
     foreign key (language_name) references languages (language_name)
+                     on delete set null
 );
 
-create table languages(
-    language_id             int NOT NULL,
-    language_name           nvarchar(100) NOT NULL,
-    compiler_invocation     nvarchar(500),
-    create_date      DATETIME DEFAULT NOW(),
-    primary key (language_id)
-);
-
-create table project_files(
+create table if not exists project_files(
     file_id         int NOT NULL,
     project_id      int,
     parent_dir      int DEFAULT NULL,
@@ -30,7 +36,9 @@ create table project_files(
     is_dir          bool DEFAULT FALSE,
     create_date      DATETIME DEFAULT NOW(),
     primary key (file_id),
-    foreign key (project_id) references projects (project_id),
+    foreign key (project_id) references projects (project_id)
+                          on delete set null,
     foreign key (parent_dir) references project_files (file_id)
+                          on delete set null
 );
 
