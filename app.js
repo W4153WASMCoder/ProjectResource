30ca8d775a98f5badf00500896d417c8e37090c9
Yell0wF1sh <86252839+Yell0wF1sh@users.noreply.github.com>
2024-10-09 21:01:03 -0400

    Initialize project

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'wasm-coder-project.c5is6oc4czys.us-east-2.rds.amazonaws.com',
+    port: '3306',
+    user: 'admin',
+    password: 'u*H1%wUv45q'
+});
+
+var app = express();
+
+connection.connect(function (err) {
+    if (!err) {
+        console.log("Database is connected ... ");
+    } else {
+        console.log("Error connecting database ... ");
+    }
+});
+
+app.get('/', (req, res, next) => {
+    console.log("Hello World!");
+});
+
+connection.query('show databases', function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+})
+
+connection.end();
+
+module.exports = app;
